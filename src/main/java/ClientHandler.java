2a288aa8325ea8e26504b52481dafa151979b997
lllagger <denizhan_yildiz@hotmail.com>
2023-03-21 00:18:06 +0300

    String fixed

--- src/main/java/ClientHandler.java
+++ src/main/java/ClientHandler.java
@@ -17,7 +17,7 @@ public class ClientHandler implements Runnable{
             this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             this.clientUsername = bufferedReader.readLine();
             clientHandlers.add(this);
-            broadcastMessage("SERVER " + clientUsername + " has entered the chat!");
+            broadcastMessage("SERVER: " + clientUsername + " has entered the chat!");
         } catch (IOException e){
             closeEverything(socket, bufferedReader, bufferedWriter);
         }
